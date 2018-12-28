5c612d48cffce260b196899a108ffa7946689670
Anguandia <mikeanguandia@gmail.com>
2018-12-28 11:52:41 +0300

    add tests for edit comment/add location routes and respective implementation methods

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,4 +2,6 @@ dat = {
     'basic': {'location': 'here', 'createdBy': 1, 'comment': 'flooding'},
     'optional': {'location': 'here', 'createdBy': 1, 'comment': 'flooding',
                  'type': 'intervention flag'},
+    'resolved': {'location': 'here', 'createdBy': 1, 'comment': 'flooding',
+                 'status': 'resolved'},
 }
