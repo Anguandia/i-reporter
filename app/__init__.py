9cb8ee8f9d283080696d9dad58659b1c2973d79d
Anguandia <mikeanguandia@gmail.com>
2018-12-28 05:29:44 +0300

    [fixes] correct reference to default config object

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask_api import FlaskAPI
-from config.config import app_config, Config
+from config.config import app_config
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__)
-    app.config.from_object('Config')
+    app = FlaskAPI(__name__, instance_relative_config=False)
+    app.config.from_object('config')
     app.config.from_object(app_config[config_name])
 
     return app
