865f21910312644da90afb07ef7934e3132dc444
Anguandia <mikeanguandia@gmail.com>
2018-12-28 05:05:45 +0300

    add welcome and create routes

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
+import os
 from flask import request, jsonify
 from .implementation import Implementation
-from run import app
+from app import create_app
+
+
+config_name = os.getenv('FLASK_ENV')
+app = create_app(config_name)
 
 
 @app.route('/')
 @app.route('/api/v1')
-@app.route('/api/v1/')
+@app.route('/api/v1')
 def home():
     return jsonify({'home': 'welcome to iReporter, please make a request'})
 
