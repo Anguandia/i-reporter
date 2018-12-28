4f85229b855e43ec56f68d03315769ccfb748358
Anguandia <mikeanguandia@gmail.com>
2018-12-28 05:33:03 +0300

    [fixes] streamline run.py so that all modules of app use same instance created in routes module

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import os
+from app.routes import app
 
-from app import create_app
-
-config_name = os.getenv('FLASK_ENV')
-app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
