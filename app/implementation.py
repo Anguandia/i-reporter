d2e6c8a803dc0a1fb7478224c1c7eb4aef582409
Anguandia <mikeanguandia@gmail.com>
2018-12-29 23:13:19 +0300

    [fixes] reorganise models and create method in implementation to reduce arguments taken by flags init method

--- app/implementation.py
+++ app/implementation.py
@@ -6,13 +6,17 @@ red_flags = {}
 
 class Implementation:
     def create(self, data):
+        others = {'type': 'red-flag', 'status': 'draft'}
         red_flag = RedFlag(
             (len(red_flags)+1), data['location'], data['createdBy'],
             data['comment']
             )
-        for field in data:
-            if field not in ['location', 'createdBy', 'comment']:
-                red_flag.__setattr__(field, data[field])
+        red_flag.__setattr__('createdOn', datetime.datetime.now())
+        for key in others:
+            if key in data:
+                red_flag.__setattr__(key, data[key])
+            else:
+                red_flag.__setattr__(key, others[key])
         red_flags[str(red_flag.id)] = red_flag.__dict__
         return [
             201, 'data', [{'id': red_flag.id, 'message': 'Created red flag'}]
