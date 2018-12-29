ce64e4ebb990374e9e0d4f2cafce18b2fc677216
Anguandia <mikeanguandia@gmail.com>
2018-12-30 01:41:06 +0300

    remove duplicate code in get/delete routes

--- app/routes.py
+++ app/routes.py
@@ -34,9 +34,12 @@ def get_flags():
     return jsonify({'Status': res[0], res[1]: res[2]}), res[0]
 
 
-@app.route('/api/v1/red_flags/<red_flag_id>', methods=['GET'])
-def get_flag(red_flag_id):
-    res = Implementation().get_flag(red_flag_id)
+@app.route('/api/v1/red_flags/<red_flag_id>', methods=['GET', 'DELETE'])
+def single_flag(red_flag_id):
+    if request.method == 'GET':
+        res = Implementation().get_flag(red_flag_id)
+    else:
+        res = Implementation().delete(red_flag_id)
     return jsonify({'Status': res[0], res[1]: res[2]}), res[0]
 
 
@@ -45,10 +48,4 @@ def get_flag(red_flag_id):
 def edit(red_flag_id, key):
     data = request.json
     res = Validation().validateEdit(data, red_flag_id, key)
-    return jsonify({'Status': res[0], res[1]: res[2]}), res[0]
-
-
-@app.route('/api/v1/red_flags/<red_flag_id>', methods=['DELETE'])
-def delete_flag(red_flag_id):
-    res = Implementation().delete(red_flag_id)
     return jsonify({'Status': res[0], res[1]: res[2]}), res[0]
